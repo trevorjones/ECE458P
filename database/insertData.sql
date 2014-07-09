a9472ccf34347d928bd7d71bf279747b2aafca69
Stephen Louli <slouli@uwaterloo.ca>
2014-07-09 16:39:23 -0400

    Added db insert script

---
+++ database/insertData.sql
@@ -0,0 +1,6 @@
+INSERT INTO Users (name, pass) VALUES('slouli', SHA2('asdf', 256));
+
+INSERT INTO Candidates (name) VALUES('Trevor Jones');
+INSERT INTO Candidates (name) VALUES('Steven Harper');
+INSERT INTO Candidates (name) VALUES('Phil Kessel');
+INSERT INTO Candidates (name) VALUES('Kyle Lowry');
