0e27b8deef1d2f07e71099db5dd58cefc4e3365b
Trevor Jones <trevorjones2k@gmail.com>
2014-07-30 14:31:43 -0400

    Updated table shit

--- database/insertData.sql
+++ database/insertData.sql
@@ -1,7 +1,7 @@
 INSERT INTO Users (name, pass) VALUES('slouli', 'asdf');
 INSERT INTO Users (name, pass) VALUES('tjones', 'asdf');
 
-INSERT INTO Candidates (id, name) VALUES('trevorjones', 'Trevor Jones');
-INSERT INTO Candidates (id, name) VALUES('stevenharper', 'Steven Harper');
-INSERT INTO Candidates (id, name) VALUES('philkessel', 'Phil Kessel');
-INSERT INTO Candidates (id, name) VALUES('kylelowry', 'Kyle Lowry');
+INSERT INTO Candidates (name) VALUES('Trevor Jones');
+INSERT INTO Candidates (name) VALUES('Steven Harper');
+INSERT INTO Candidates (name) VALUES('Phil Kessel');
+INSERT INTO Candidates (name) VALUES('Kyle Lowry');
